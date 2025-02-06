9784ee4afda8a1adab63628b4099f70b195189b3
AugustOstereje <202408669@post.au.dk>
2025-02-06 14:06:51 +0100

    Update combat_medic.cpp

--- wargame_cpp/src/combat_medic.cpp
+++ wargame_cpp/src/combat_medic.cpp
@@ -2,8 +2,18 @@
 
 CombatMedic::CombatMedic(int health, int damage) : Soldier(health, damage)
 {
+    
 }
 
 void CombatMedic::heal(Unit &target)
 {
+    if (target.get_health() < 0)
+    {
+        std::cout << "He is gone";
+    }
+    else
+    {
+        printf("Hang in there!");
+        unit_set_health(target, unit_get_health(target) + 10);
+    }
 }
