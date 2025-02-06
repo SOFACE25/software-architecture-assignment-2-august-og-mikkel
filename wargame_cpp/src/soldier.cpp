020af89546572105dd639b9b8cd05f67d21fa992
AugustOstereje <202408669@post.au.dk>
2025-02-06 15:13:27 +0100

    done
    done

--- wargame_cpp/src/soldier.cpp
+++ wargame_cpp/src/soldier.cpp
@@ -1,13 +1,17 @@
 #include "soldier.hpp"
 
+// Soldier-constructor
 Soldier::Soldier(int health, int damage) : Unit(health), damage(damage)
 {
 }
 
 void Soldier::attack(Unit &target)
 {
-    target.take_hit(this->damage);
+    target.take_hit(this->damage); //using inhereted target function take_hit 
 }
+
+
+//Soldiers cant heal, can be deleted...
 void Soldier::heal(Unit &target)
 {
     
